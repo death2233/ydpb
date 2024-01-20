2f585799aef1673cac484025cc4bb7edc4ee90c3
tony <deathmid2233@gmail.com>
2024-01-20 16:08:41 +0700

    检查进程是否在运行

--- check_process.sh
+++ check_process.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+pid=$(pgrep -f "geneva.py")
+
 # 检查进程是否在运行
-if pgrep -f "geneva.py" > /dev/null
+if [ -n "$pid" ];
 then
-    echo "geneva.py 正在运行."
+    echo "geneva.py 正在运行. PID: $pid"
 else
     echo "geneva.py 未在运行. 正在尝试重新启动..."
     # 在此处添加重新启动命令，例如：
-     nohup python3 /root/ydpb/geneva.py -q 100 -w 4 &
+    nohup python3 /root/ydpb/geneva.py -q 100 -w 4 &
+    pid=$(pgrep -f "geneva.py")
+    echo "geneva.py 正在运行. PID: $pid"
 fi
--- start_geneva.sh
+++ start_geneva.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-# 克隆仓库
-sudo git clone https://github.com/death2233/ydpb /root/ydpb
-chmod 777 /root/ydpb/*.sh
+REPO_DIR="/root/ydpb"
+
+# 检查仓库是否存在
+if [ -d "$REPO_DIR" ]; then
+    # 仓库存在，执行 git pull 来更新
+    cd "$REPO_DIR" || exit
+    git pull
+else
+    # 仓库不存在，执行 git clone
+    sudo git clone https://github.com/death2233/ydpb "$REPO_DIR"
+fi
+
+# 授予执行权限
+chmod 777 "$REPO_DIR"/*.sh
 
 # 安装依赖
 yum install -y python3 python3-devel gcc gcc-c++ git libnetfilter* libffi-devel
@@ -15,6 +26,6 @@ iptables -A OUTPUT -p tcp --sport 443 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j
 iptables -A INPUT -p tcp --dport 443 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
 
 # 启动脚本
-nohup python3 /root/ydpb/geneva.py -q 100 -w 4 > geneva.log 2>&1 &
+"$REPO_DIR"/check_process.sh
 
-echo "Geneva启动中..."
+echo "start_geneva 执行完毕..."
