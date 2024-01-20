5cfcb69e1409fc14e83ae760f043c9373ec4f94a
tony <deathmid2233@gmail.com>
2024-01-20 15:55:58 +0700

    如果要开放其他端口请自行添加

--- start_geneva.sh
+++ start_geneva.sh
@@ -8,6 +8,12 @@ yum install -y python3 python3-devel gcc gcc-c++ git libnetfilter* libffi-devel
 pip3 install --upgrade pip
 pip3 install scapy netfilterqueue
 
+# 默认开放 80 443 如果要开放其他端口请自行添加
+iptables -A OUTPUT -p tcp --sport 80 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
+iptables -A INPUT -p tcp --dport 80 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
+iptables -A OUTPUT -p tcp --sport 443 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
+iptables -A INPUT -p tcp --dport 443 --tcp-flags SYN,RST,ACK,FIN,PSH SYN,ACK -j NFQUEUE --queue-num 100
+
 # 启动脚本
 nohup python3 /root/ydpb/geneva.py -q 100 -w 4 > geneva.log 2>&1 &
 
